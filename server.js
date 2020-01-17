e3eb6bd94e78582c6458e2296d13d8ff2112f0b2
José Antonio Muñoz Jiménez <jamj2000@gmail.com>
2020-01-17 22:36:22 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const apiRoutes = require('./routes');
 
 const app = express();
 
@@ -7,6 +8,7 @@ mongoose.connect("mongodb://localhost:27017/tienda", { userNewUrParser: true})
     .then( db => console.log("Conexión a BD correcta"))
     .catch(err => console.log("Error al conectar a BD: " + err));
 
-
+app.use(express.json());    // IMPORTANTE: Poner esto antes de las rutas
+app.use('/api', apiRoutes);
 
 app.listen(3000, () => console.log("Servidor iniciado..."));
