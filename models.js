87226a2a870af5abb071cd1fa9ef7c2dba091cdc
Gloria <you@example.com>
2020-01-17 10:56:50 +0100

    models act1

--- models.js
+++ models.js
@@ -1,3 +1,7 @@
 const mongoose = require('mongoose');
-const Cliente = mongoose.model('Cliente',);
-const Articulo = mongoose.model('Articulo',);
+const Cliente = mongoose.model('Cliente', 
+    {nombre: String, apellidos: String}
+);
+const Articulo = mongoose.model('Articulo',
+{nombre: String, precio: Number}
+);
