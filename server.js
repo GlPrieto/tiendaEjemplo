9a86c4e35e8715f386cb336aeb5f6c41dcbbf955
Gloria <you@example.com>
2020-01-31 12:37:59 +0100

    cambios

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const apiRoutes = require('./routes');
@@ -6,7 +7,7 @@ const app = express();
 
 
 // CONEXIÓN A BASE DE DATOS
-mongoose.connect("mongodb+srv://gloria:gloria@cluster0-v6pcu.mongodb.net/tienda?retryWrites=true&w=majority", { useNewUrlParser: true })
+mongoose.connect(process.env.DB_URI, { useNewUrlParser: true })
     .then(db => console.log("Conexión a BD correcta"))
     .catch(error => console.log("Error al conectarse a la BD" + error));
 
